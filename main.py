a4d3fbcf3edc05ab6394692ec09622f57e42f2cc
Karina Nerone <karinanerone@gmail.com>
2024-09-16 17:27:40 -0300

    Added exponentiation function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Projeto: Calculadora Aluna: Karina Nerone
 def calculadora():
-    operacao = input("Escolha a operação a realizar (+, -, *, /): ")
+    operacao = input("Escolha a operação a realizar (+, -, *, /, **): ")
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
 
@@ -16,6 +16,9 @@ def calculadora():
     elif operacao == "/":
         resultado = num1 / num2
         print("O resultado da divisão é:", resultado)
+    elif operacao == "**":
+        resultado = num1 ** num2
+        print("O resultado da exponenciação é:", resultado)
     else:
         print("Operação inválida!")
 
